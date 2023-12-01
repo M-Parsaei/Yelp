c95f691f7d71789c737b4c570669fa68c9a92c37
M-Parsaei <khodadostetdarm@gmail.com>
2023-12-01 05:50:12 -0700

    added exit button

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pyodbc
 from databaseConnection import dbStartConnection,dbCloseConnection, DBError
 import authenticate,contorllers
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtWidgets
+import sys
+from userInterface.login_page import Ui_Form
+
+# a boolean that defines whether the program should end or not 
+is_program_over = False
 
 def main():
     try:
@@ -18,14 +25,29 @@ def main():
         #else: 
         #    print("Logged In...")
 
-        result = contorllers.searchTable(filter={"name":"john"},
+
+        #setting up the login user interface 
+        app = QApplication(sys.argv)
+        Form = QtWidgets.QWidget()
+        ui = Ui_Form()
+        ui.setupUi(Form)
+        Form.show()
+        
+
+
+        '''result = contorllers.searchTable(filter={"name":"john"},
                                    tableName="user_yelp",
                                    cursor=dbCursor)
 
-        print(result)
-        dbCloseConnection(connection)
+        print(result)'''
+        sys.exit(app.exec_())
+
     except DBError as e:
         print(f"Error: {e}")
+
+
+def endProgram(connection: pyodbc.Connection):
+    dbCloseConnection(connection)
 
 if (__name__ == '__main__'):
     main()
